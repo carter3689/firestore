4418b41f2a29c17b2fda3a3b6443c0409a8561eb
Joel Carter <carter3689@gmail.com>
2018-02-09 01:38:54 -0600

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+//Angular Fire Imports
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
 
 import { AppComponent } from './app.component';
 
+//FormsModule
+import { FormsModule } from '@angular/forms';
+
+
+// Firebase Creds
+
+var firebaseConfig = {
+    apiKey: "AIzaSyD75l61-_V5NPmhEzWG9VMKGMvX8evc_18",
+    authDomain: "firestore-6eaaa.firebaseapp.com",
+    databaseURL: "https://firestore-6eaaa.firebaseio.com",
+    projectId: "firestore-6eaaa",
+    storageBucket: "firestore-6eaaa.appspot.com",
+    messagingSenderId: "91352741196"
+  };
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFirestoreModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
